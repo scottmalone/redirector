8fea6f3daf69dd95e9f5e28777dc003dec9c7beb
Scott Malone <scottmalone101@gmail.com>
2019-04-23 22:40:29 -0600

    Adds model and factory annotation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
   has_many :short_links
 
--- spec/factories/original_links.rb
+++ spec/factories/original_links.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: original_links
+#
+#  id                :bigint(8)        not null, primary key
+#  original_url      :string           not null
+#  short_links_count :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+# Indexes
+#
+#  index_original_links_on_original_url  (original_url) UNIQUE
+#
+
 FactoryBot.define do
   factory :original_link do
     original_url { Faker::Internet.url }
